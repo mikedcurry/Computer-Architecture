0e7c47bad6d65bc92c3aa9a55bf1b18fd22d5f5e
Michael Curry <mdcurry1138@gmail.com>
2020-04-22 19:29:33 -0400

    MVP day3

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -10,19 +10,21 @@ class CPU:
         """Construct a new CPU."""
         self.ram = [0] * 256 # Total RAM 
         self.pc = 0
-        self.registry = [0] * 8             # Program Counter: address of the currently executing instruction
+        self.registry = [0] * 8
+
+        # Program Counter: address of the currently executing instruction
         # self.ir = [0]  # self.ram[pc]     # Instruction Register: copy of the currently executing instruction
         # self.mar = [0] * 8      # Memory Address Registry: holds the memory address we're reading or writing
         # self.mdr = [0] * 8      # Memory Data Register: holds the value to write or read
         # self.fl = [0] * 8       # Flags: hold current "flag" status
       
 
-    def load(self, program_filename):
+    def load(self): # , program_filename):
         """Load a program into memory."""
 
         address = 0
 
-        with open(program_filename) as f:
+        with open(sys.argv[1]) as f:
 	        for line in f:
 		        line = line.split('#')
 		        line = line[0].strip()
@@ -68,11 +70,11 @@ class CPU:
     def ram_write(self, address, value):
         self.ram[address] = value
 
-    def HLT():
-        # HLT = 0b00000001
-        # if self.ir == 0b00000001:
-        #     return running = False
-        pass
+    # def HLT():
+    #     # HLT = 0b00000001
+    #     # if self.ir == 0b00000001:
+    #     #     return running = False
+    #     pass
 
     def trace(self):
         """
@@ -94,12 +96,16 @@ class CPU:
 
         print()
 
-    def run(self, program_filename):
+    def run(self): #, program_filename):
         """Run the CPU."""
         # I assume this will get more robust... should load a particular thing...
-        self.load(program_filename)
+        # self.load(sys.argv[1]) #program_filename)
         
         self.pc = 0
+
+        # Stack Pointer (SP) value lives at registry[7]
+        SP = 7
+        self.registry[SP] = 0xF4 # starts at the top less extra room for other stuff
 
         # For now, put the Opcode values direction in the run function...
         # Maybe eventually store them in a dictionary(?) / hashtable?
@@ -107,6 +113,8 @@ class CPU:
         PRN = 0b01000111
         HLT = 0b00000001
         MUL = 0b10100010
+        PUSH = 0b01000101
+        POP = 0b01000110
 
         running = True
 
@@ -131,9 +139,40 @@ class CPU:
                 self.registry[operand_a] *= self.registry[operand_b]
                 self.pc += 3
 
+            elif ir == PUSH:
+                # decriment the stack pointer
+                self.registry[SP] -= 1
+
+                # Copy value from registry (called at PC + 1) into stack/RAM/memory
+                value = self.registry[operand_a] # value from PC+1 from program
+                address = self.registry[SP]      # index of stack to push value to
+
+                self.ram[address] = value        # Write/push value to stack in ram/memory
+
+                # Move pointer up through program
+                self.pc += 2
+
+            elif ir == POP:
+                # Copy value from stack/RAM/memory to registry
+                address = self.registry[SP] # looking for the address SP is at
+
+                value = self.ram[address] # get value at SP address
+
+                self.registry[operand_a] = value # set registry from PC+1 to value from mem
+
+                self.registry[SP] += 1 # Oh yeah... this. XD
+
+                self.pc += 2 # etc...
+
             elif ir == HLT:
                 running = False
 
             else:
                 print('Error: unknown instructions')
                 running = False
+
+
+# print(sys.argv[1])
+# thing = CPU()
+
+# thing.run(sys.argv[1])
